1b9ed7be31f4332aaa74c04ddb45c937cb176169
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-28 00:50:27 +0000

    Update urls.py

--- ecommerce_api/store/urls.py
+++ ecommerce_api/store/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, OrderViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import ProductListCreateView, ProductDetailView
+
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
 router.register(r'orders', OrderViewSet)
@@ -10,4 +12,7 @@ urlpatterns = [
     path('', include(router.urls)),  
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
+
