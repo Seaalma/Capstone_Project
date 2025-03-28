58bcfc6b2ba07aaccac465611661a352933a2f89
Seelma <selmaelmaaroufi9@gmail.com>
2025-03-28 00:49:00 +0000

    Update views.py

--- ecommerce_api/store/views.py
+++ ecommerce_api/store/views.py
@@ -2,12 +2,22 @@ from rest_framework import viewsets
 from .models import Product, Order
 from .serializers import ProductSerializer, OrderSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]  # Read access for everyone, write access for authenticated users
+class ProductListCreateView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
